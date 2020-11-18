3fd8f544024cfaebdd152c687f5c65e85a7150d0
pedroxian <44950875+pedroxian@users.noreply.github.com>
2020-11-18 15:35:10 +0100

    Add files via upload

---
+++ duplex/conformer.java
@@ -0,0 +1,9 @@
+sarge.rom /parrot.os 4.10 false.remoter application.OPENFORM CONFECT.BACKBOX -BLACKARCH.RUNNING/PROCEDURE
+    SAT.COM eject.disk /rom cage .activate cups.js /root.eclection.python 3.7 -RUNTIME  CAPSULE.CATAPULT RANGE.7KM
+
+        {
+            WALLSCH:REMOTE USE LINK.SERVER.2 -UPLINK .SERVING.JS    section.vb use.killprogress
+            made.heap -caunty lord.far /fad.detect item/lab category.room/sampler.discouse fujitsu.siop=roob.2.7 .-self
+        }       KLODE.ARRANGER TIMECOP.KT /USE.FORCE    bruteforce.is analitics.python/token.raw
+
+            mode.raw /diskouse made have <java>
---
+++ duplex/mento.vb
@@ -0,0 +1,10 @@
+MENTO.REVALID EXECUTABLES.LIST RUN.DEBUG -OPTION:rescue .recovery.task win.option -other.drives
+
+{
+    SUCH.LINK sergent.mo / ruit.runner - speccy.duplex priority.xeon 2.0 -zen.2.0
+        advanced.micro.devices -option.js /run operative -exce /cunt.form -center .hold
+
+            MOUTH.RETAIL EXEC GOV.ROMAIN    pulse.generator .d2=h-key.app .center
+
+                moving.link item.update ded.links/secure ip.military.base -open.port:8080
+}
